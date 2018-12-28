ccc83ba73a22a808aff5bc53d6d9a2f77d06e7e8
jingwu <jingwu@vip.163.com>
2018-12-28 12:11:26 +0800

    beanstalk

--- beanstalk/beanstalk.go
+++ beanstalk/beanstalk.go
@@ -7,20 +7,28 @@ import (
     "github.com/spf13/viper"
 )
 
-//var conn *Bclient.Conn
+var err error
+var conn *Bclient.Conn
 
 func Put(tubeName string, body []byte) (uint64, error) {
-    conn, err := Conn()
-    if err != nil {
-        fmt.Println(err)
+    //fmt.Println("conn start")
+    //fmt.Println(conn)
+    //fmt.Println("conn end")
+    if conn == nil {
+        conn, err = Conn()
+        if err != nil {
+            fmt.Println(err)
+        }
     }
     tube := &Bclient.Tube{conn, tubeName}
     id, err := tube.Put(body, 1, 0, 30*time.Second)
+    //conn.Close()
     return id,err
 }
 
 func Conn() (*Bclient.Conn, error) {
-    return Bclient.Dial("tcp", viper.GetString("beanstalk.host") + ":" + viper.GetString("beanstalk.port"))
+    conn, err := Bclient.Dial("tcp", viper.GetString("beanstalk.host") + ":" + viper.GetString("beanstalk.port"))
+    return conn, err
 }
 
 func init() {
