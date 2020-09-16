93d2be0c3010471f4483375b566a032a39b64911
jingwu <jingwu@vip.163.com>
2020-09-16 08:32:59 +0800

    支持Hdel命令，支持原生方式执行命令，支持新的配置方式

--- redis/redis.go
+++ redis/redis.go
@@ -2,7 +2,9 @@ package redis
 
 import (
 	"fmt"
-//    "reflect"
+	"strconv"
+	"strings"
+	//    "reflect"
 	"github.com/gomodule/redigo/redis"
 	"github.com/jingwu15/golib/tconv"
 	"github.com/jingwu15/golib/time"
@@ -16,11 +18,142 @@ const TIMEOUT_WRITE int = 1
 const POOL_MAX_IDLE int = 16
 const POOL_MAX_ACTIVE int = 16
 
+var configs = map[string]Config{}
+
+type Config struct {
+	Ukey               string
+	Raw                map[string]interface{}
+	ConnMethod         string // connect/sentinel/cluster
+	Address            string
+	SentinelAddress    []string
+	SentinelMasterName string
+	ClusterAddress     []string
+	Password           string
+	Timeout            int
+	Db                 int
+	Cmdlog             string
+	Conn               redis.Conn
+	//"address":             "172.16.100.112:6379",
+	//"sentinel_address":    "172.16.100.112:26379, 172.16.100.112:26380, 172.16.100.112:26381",
+	//"sentinel_mastername": "mymaster",
+	//"cluster_address":     "172.16.100.112:26379, 172.16.100.112:26380, 172.16.100.112:26381",
+	//"password":            "Gmck7X02",
+	//"timeout":             "0",
+	//"db":                  "1",
+	//"cmdlog":              "off",
+}
+
+func ParseConfig(ukey string, cfg map[string]interface{}) (Config, error) {
+	config := Config{}
+	config.Ukey = ukey
+	config.Raw = cfg
+	if password, ok := cfg["password"].(string); ok {
+		config.Password = password
+	}
+	if db, ok := cfg["db"].(string); ok {
+		dbInt, _ := strconv.Atoi(db)
+		config.Db = dbInt
+	}
+	if cmdlog, ok := cfg["cmdlog"].(string); ok {
+		config.Cmdlog = string(cmdlog)
+	}
+	if address, ok := cfg["address"].(string); ok {
+		config.Address = address
+		config.ConnMethod = "connect"
+	}
+	if sentinelAddress, ok := cfg["sentinel_address"].(string); ok {
+		config.SentinelAddress = strings.Split(sentinelAddress, ",")
+		for i, r := range config.SentinelAddress {
+			config.SentinelAddress[i] = strings.TrimSpace(r)
+		}
+		config.ConnMethod = "sentinel"
+	}
+	if sentinelMasterName, ok := cfg["sentinel_mastername"].(string); ok {
+		config.SentinelMasterName = sentinelMasterName
+	}
+	if clusterAddress, ok := cfg["cluster_address"].(string); ok {
+		config.ClusterAddress = strings.Split(clusterAddress, ",")
+		for i, r := range config.ClusterAddress {
+			config.ClusterAddress[i] = strings.TrimSpace(r)
+		}
+		config.ConnMethod = "cluster"
+	}
+
+	return config, nil
+}
+
+func SetCfg(ukey string, cfg map[string]interface{}) (err error) {
+	configs[ukey], err = ParseConfig(ukey, cfg)
+	return err
+}
+
+func GetClient(ukey string) (c Client, err error) {
+	config, ok := configs[ukey]
+	if !ok {
+		return Client{}, fmt.Errorf("配置不存在")
+	}
+	if config.ConnMethod == "connect" {
+		c, err := Connect(config.Address, config.Password, config.Timeout, config.Db)
+		return Client{Conn: c, Address: config.Address, Auth: config.Password, Db: config.Db}, err
+	}
+	if config.ConnMethod == "sentinel" {
+		c, err := ConnectSentinel(config.SentinelMasterName, config.SentinelAddress, config.Password, config.Timeout, config.Db)
+		return Client{Conn: c, SentinelMasterName: config.SentinelMasterName, SentinelAddress: config.SentinelAddress, Auth: config.Password, Db: config.Db}, err
+	}
+	if config.ConnMethod == "cluster" {
+		return Client{}, err
+	}
+	return Client{}, fmt.Errorf("配置错误")
+}
+
+func Connect(addr string, password string, db int, timeout int) (c redis.Conn, err error) {
+	c, err = redis.DialTimeout("tcp", addr, time.Keep(timeout), time.Keep(timeout), time.Keep(timeout))
+	if err != nil {
+		return c, err
+	}
+	if password != "" {
+		c.Do("AUTH", password)
+	}
+	c.Do("select", db)
+	return c, nil
+}
+
+func ConnectSentinel(masterName string, addrs []string, password string, db int, timeout int) (c redis.Conn, err error) {
+	//timeout, timeourRead, timeoutWrite
+	cinfo := []string{}
+	for _, addr := range addrs {
+		conn, err := redis.DialTimeout("tcp", addr, time.Keep(timeout), time.Keep(timeout), time.Keep(timeout))
+		if err == nil {
+			cinfo, err = redis.Strings(conn.Do("SENTINEL", "get-master-addr-by-name", masterName))
+			if err == nil {
+				break
+			}
+		}
+	}
+	if err != nil {
+		return c, fmt.Errorf("redigo: no sentinels available, error: %s", err.Error())
+	}
+	addr := fmt.Sprintf("%s:%s", cinfo[0], cinfo[1])
+	c, err = redis.DialTimeout("tcp", addr, time.Keep(timeout), time.Keep(timeout), time.Keep(timeout))
+	if err != nil {
+		return c, err
+	}
+	if password != "" {
+		c.Do("AUTH", password)
+	}
+	c.Do("select", db)
+	return c, nil
+}
+
 //单个客户端连接
 type Client struct {
-	Conn redis.Conn
-	Addr string
-	Auth string
+	Conn               redis.Conn
+	Address            string
+	SentinelAddress    []string
+	SentinelMasterName string
+	ClusterAddress     []string
+	Auth               string
+	Db                 int
 }
 
 //单个连接池
@@ -30,17 +163,6 @@ type Pool struct {
 	Auth      string
 	MaxIdle   int
 	MaxActive int
-}
-
-//哨兵, 主从
-type Sentinel struct {
-	Master       string         //master name
-	Addrs        []string       //哨兵地址
-	CAddrs       map[string]int //当前正在连接的客户端          {"127.0.0.1:6379": 0}
-	Auth         string         //密码
-	TimeoutConn  int            //连接超时
-	TimeoutRead  int            //读取超提
-	TimeoutWrite int            //写入超时
 }
 
 //-------------------------------------连接相关的操作-------------------------------------------------
@@ -84,10 +206,10 @@ func New(cfg map[string]string) (client Client, err error) {
 
 	c, err := redis.DialTimeout("tcp", addr, time.Keep(toc), time.Keep(tor), time.Keep(tow))
 	if err != nil {
-		return Client{Addr: addr}, err
+		return Client{Address: addr}, err
 	} else {
 		c.Do("AUTH", cfg["auth"])
-		return Client{Conn: c, Addr: addr, Auth: cfg["auth"]}, nil
+		return Client{Conn: c, Address: addr, Auth: cfg["auth"]}, nil
 	}
 }
 
@@ -118,7 +240,7 @@ func NewPool(cfg map[string]string, cfgs ...map[string]int) Pool {
 func (p Pool) Get() Client {
 	c := p.Pool.Get()
 	c.Do("AUTH", p.Auth)
-	return Client{Conn: c, Addr: p.Addr, Auth: p.Auth}
+	return Client{Conn: c, Address: p.Addr, Auth: p.Auth}
 }
 
 //关闭连接池中
@@ -126,151 +248,113 @@ func (p Pool) Close() {
 	p.Pool.Close()
 }
 
-//新建哨钱
-func NewSentinel(master string, items []map[string]string, cfgs ...map[string]interface{}) Sentinel {
-	addrs := []string{}
-	for _, row := range items {
-		addrs = append(addrs, fmt.Sprintf("%s:%s", row["host"], row["port"]))
-	}
-	s := Sentinel{Master: master, Addrs: addrs, Auth: "", TimeoutConn: TIMEOUT_CONN, TimeoutRead: TIMEOUT_READ, TimeoutWrite: TIMEOUT_WRITE}
-	if v, ok := cfgs[0]["auth"]; ok {
-		s.Auth = v.(string)
-	}
-	if v, ok := cfgs[0]["timeout_conn"]; ok {
-		s.TimeoutConn = v.(int)
-	}
-	if v, ok := cfgs[0]["timeout_read"]; ok {
-		s.TimeoutRead = v.(int)
-	}
-	if v, ok := cfgs[0]["timeout_write"]; ok {
-		s.TimeoutWrite = v.(int)
-	}
-	return s
-}
-
-//哨兵取一个连接，未来实现轮询，权重，Hash方式
-func (s Sentinel) Get() (Client, error) {
-	var err error
-	cinfo := []string{}
-	for _, addr := range s.Addrs {
-		conn, err := redis.DialTimeout("tcp", addr, time.Keep(s.TimeoutConn), time.Keep(s.TimeoutRead), time.Keep(s.TimeoutWrite))
-		if err == nil {
-			cinfo, err = redis.Strings(conn.Do("SENTINEL", "get-master-addr-by-name", s.Master))
-			if err == nil {
-				break
-			} else {
-				fmt.Println("cinfo------fail", err, cinfo)
-			}
-		}
-	}
-	if err != nil {
-		return Client{}, fmt.Errorf("redigo: no sentinels available, error: %s", err.Error())
-	}
-	addr := fmt.Sprintf("%s:%s", cinfo[0], cinfo[1])
-	c, err := redis.DialTimeout("tcp", addr, time.Keep(s.TimeoutConn), time.Keep(s.TimeoutRead), time.Keep(s.TimeoutWrite))
-	if err != nil {
-		return Client{}, err
-	}
-	if s.Auth != "" {
-		c.Do("AUTH", s.Auth)
-	}
-	return Client{Conn: c, Addr: addr}, nil
-}
-
-////哨兵取一个连接池, 未来实现轮询，权重，Hash方式，暂不实现
-//func (s Sentinel) GetPool() (Pool, error) {
-//}
-
 //-------------------------------------普通K-V操作-------------------------------------------------
 func (c Client) Set(key string, value string) (string, error) {
-	return FStr(c.do("SET", key, value))
+	return FStr(c.Do("SET", key, value))
 }
 
 func (c Client) Get(key string) (string, error) {
-	return FStr(c.do("GET", key))
+	return FStr(c.Do("GET", key))
 }
 
 func (c Client) Mget(keys []string) ([]string, error) {
-    ks := make([]interface{}, len(keys))
-    for i, v := range keys { ks[i] = v }
-    return FStrs(c.do("MGET", ks...))
+	ks := make([]interface{}, len(keys))
+	for i, v := range keys {
+		ks[i] = v
+	}
+	return FStrs(c.Do("MGET", ks...))
 }
 
 //-------------------------------------Hash(哈希表)操作-------------------------------------------------
 func (c Client) Hset(key string, hkey string, value string) (int, error) {
-	return FInt(c.do("HSET", key, hkey, value))
+	return FInt(c.Do("HSET", key, hkey, value))
 }
 
 func (c Client) Hgetall(key string) (map[string]string, error) {
-	return FMapStrStr(c.do("HGETALL", key))
+	return FMapStrStr(c.Do("HGETALL", key))
 }
 
 func (c Client) Hget(key string, hkey string) (string, error) {
-	return FStr(c.do("HGET", key, hkey))
+	return FStr(c.Do("HGET", key, hkey))
 }
 
 func (c Client) Hincrby(key string, hkey string, incr int) (int, error) {
-    return c.Hincr(key, hkey, incr)
+	return c.Hincr(key, hkey, incr)
 }
 
 func (c Client) Hincr(key string, hkey string, incr int) (int, error) {
-    return FInt(c.do("HINCRBY", key, hkey, incr))
+	return FInt(c.Do("HINCRBY", key, hkey, incr))
 }
 
 func (c Client) Hincrbyfloat(key string, hkey string, incr float64) (float64, error) {
-    return c.HincrFloat(key, hkey, incr)
+	return c.HincrFloat(key, hkey, incr)
 }
 
 func (c Client) HincrFloat(key string, hkey string, incr float64) (float64, error) {
-	return FFloat(c.do("HINCRBYFLOAT", key, hkey, incr))
+	return FFloat(c.Do("HINCRBYFLOAT", key, hkey, incr))
 }
 
-func (c Client) Hmget(key string, hkeys []string) (resp map[string]string, err error) {
-    ks := []interface{}{key}
-    for _, v := range hkeys { ks = append(ks, v) }
-    rs, err := FBytess(c.do("HMGET", ks...))
-    if err != nil { return resp, err }
+func (c Client) Hmget(key string, hkeys ...string) (resp map[string]string, err error) {
+	ks := []interface{}{key}
+	for _, v := range hkeys {
+		ks = append(ks, v)
+	}
+	rs, err := FBytess(c.Do("HMGET", ks...))
+	if err != nil {
+		return resp, err
+	}
 
-    resp = map[string]string{}
-    for i, hkey := range hkeys {
-        if len(rs[i]) > 0 {
-            resp[hkey] = string(rs[i])
-        }
-    }
-    return resp, err
+	resp = map[string]string{}
+	for i, hkey := range hkeys {
+		if len(rs[i]) > 0 {
+			resp[hkey] = string(rs[i])
+		}
+	}
+	return resp, err
+}
+
+func (c Client) Hdel(key string, hkeys ...string) (int, error) {
+	ks := []interface{}{key}
+	for _, v := range hkeys {
+		ks = append(ks, v)
+	}
+	return FInt(c.Do("HDEL", ks...))
 }
 
 //------------------------------------------------List(列表)操作-----------------------------------
 func (c Client) Lindex(key string, index string) (string, error) {
-	return FStr(c.do("LINDEX", key, index))
+	return FStr(c.Do("LINDEX", key, index))
 }
 
 func (c Client) Llen(key string) (int, error) {
-	return FInt(c.do("LLEN", key))
+	return FInt(c.Do("LLEN", key))
 }
 
 func (c Client) Lpop(key string) (string, error) {
-	return FStr(c.do("LPOP", key))
+	return FStr(c.Do("LPOP", key))
 }
 
 func (c Client) Rpush(key string, values ...string) (string, error) {
-	//var resp []byte
-	//args := []interface{}{key}
-	//for _,v := range values {
-	//	args = append(args, v)
-	//}
-	result, err := c.do("RPUSH", values)
+	result, err := c.Do("RPUSH", values)
 	return FStr(result, err)
 }
 
 //-----------------------------------------redis连接封-----------------------------------------------
 
-func (c Client) do(cmd string, args ...interface{}) (result interface{}, err error) {
+func (c Client) Do(cmd string, args ...interface{}) (result interface{}, err error) {
 	result, err = c.Conn.Do(cmd, args...)
 	if err != nil {
 		log.Error(fmt.Sprintf("redis error[%s]\n", err))
 	}
 	return result, err
+}
+
+func Strs2Ifs(keys ...string) []interface{} {
+	ks := []interface{}{}
+	for _, v := range keys {
+		ks = append(ks, v)
+	}
+	return ks
 }
 
 //------------------------------------------数据类型转换---------------------------------------------
@@ -303,7 +387,7 @@ func FFloat(result interface{}, err error) (float64, error) {
 		return redis.Float64(result, err)
 	} else {
 		return result.(float64), err
-    }
+	}
 }
 
 func FStr(result interface{}, err error) (string, error) {
@@ -314,10 +398,12 @@ func FStr(result interface{}, err error) (string, error) {
 	}
 }
 func FStrs(result interface{}, err error) ([]string, error) {
-    rows, err := FBytess(result, err)
-    rs := []string{}
-    for _, row := range rows { rs = append(rs, string(row)) }
-    return rs, err
+	rows, err := FBytess(result, err)
+	rs := []string{}
+	for _, row := range rows {
+		rs = append(rs, string(row))
+	}
+	return rs, err
 }
 func FMapStrStr(result interface{}, err error) (map[string]string, error) {
 	if err == nil {
